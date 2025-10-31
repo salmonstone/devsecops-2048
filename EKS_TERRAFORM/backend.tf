1727a8a486f59743c6833d500226c4b398857413
Aryan Singh CHAUHAN <arysingh9832@gmail.com>
2025-10-31 17:00:13 +0530

    Update backend.tf

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "devsecops-2048-aryan" # Replace with your actual S3 bucket name
+    bucket = "devsecops-2048-aryan-yourname" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "eu-north-1"
   }
